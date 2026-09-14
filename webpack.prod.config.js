678427570539bfca1a6255a6abcf4a411c7d831f
Deepak <deepucsait@gmail.com>
2026-09-14 14:43:05 +0530

    configured mfe

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,6 +1,18 @@
-module.exports = require('./webpack.config');
-module.exports.mode = 'production';
-module.exports.devtool = false;
-module.exports.remotes = {
-  wordToPdf: 'https://deepak-devadiga.github.io/word-to-pdf-remote/remoteEntry.js',
-};
+const {
+  shareAll,
+  withModuleFederationPlugin,
+} = require('@angular-architects/module-federation/webpack');
+
+module.exports = withModuleFederationPlugin({
+  remotes: {
+    wordToPdf: 'https://deepak-devadiga.github.io/word-to-pdf-remote/remoteEntry.js',
+  },
+
+  shared: {
+    ...shareAll({
+      singleton: true,
+      strictVersion: true,
+      requiredVersion: 'auto',
+    }),
+  },
+});
