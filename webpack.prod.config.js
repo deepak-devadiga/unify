37ec68d38f1c60d7789539ded115ae5533771912
Deepak <deepucsait@gmail.com>
2026-09-14 14:33:03 +0530

    Added mfe

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,1 +1,6 @@
 module.exports = require('./webpack.config');
+module.exports.mode = 'production';
+module.exports.devtool = false;
+module.exports.remotes = {
+  mfe1: 'https://deepak-devadiga.github.io/word-to-pdf-remote/remoteEntry.js',
+};
